4e2a1851f7986374b784476a2a8f2e7e3819ce2e
gustavdlima <gustavdlima@gmail.com>
2024-11-14 21:13:39 -0300

    feat: create header file ft_ping.h with function declarations

---
+++ includes/ft_ping.h
@@ -0,0 +1,37 @@
+#ifndef ft_ping_H
+#define ft_ping_H
+
+typedef enum {
+	False = 0,
+	True = 1
+} Boolean;
+
+// typedef struct {
+// 	RomanNumeral roman_numeral;
+// 	int value;
+// 	char symbol;
+// } UniqueRomanNumeralMap;
+
+// typedef struct {
+// 	RomanNumeral roman_numeral;
+// 	int value;
+// 	char *symbol;
+// } RomanNumeralMap;
+
+// #define TOTAL_ROMAN_NUMERALS 13
+// #define TOTAL_UNIQUE_ROMAN_NUMERALS 7
+// #define MAX_ROMAN_NUMERAL_LENGTH 3999
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+// #include "validations.h"
+// #include "utils.h"
+// #include "convert.h"
+// #include "cli.h"
+
+
+
+
+#endif
